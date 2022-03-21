2baf9662d54e2d34f236dc7e46418906b1353e5a
Vladislav Ketris <vketrys@gmail.com>
2022-03-21 12:19:11 +0300

    Fixed all comments from PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
