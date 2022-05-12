785a0f2aec2e54980a2c530ba34e38adfb19cd08
Vladislav Ketris <vketrys@gmail.com>
2022-05-12 16:43:52 +0300

    Working on Character Info page script and style

--- src/pages/info/CharacterInfo.tsx
+++ src/pages/info/CharacterInfo.tsx
@@ -1,26 +1,34 @@
-import Button from 'components/button/Button';
-import EpisodeCard from 'components/card/EpisodeCard';
-import paths from 'components/navigation/paths';
-import axios from 'config/axios';
-import useTypeSelector from 'hooks/useTypeSelector';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useNavigate, useParams } from 'react-router-dom';
+
+import Button from 'components/button/Button';
+import EpisodeCard from 'components/card/EpisodeCard';
+import paths from 'components/navigation/paths';
+
+import axios from 'config/axios';
+import useTypeSelector from 'hooks/useTypeSelector';
 import { EpisodeProps } from 'types/episodeTypes';
+
 import style from './CharacterInfo.module.scss';
 
 type CharacterParams = {
   id: string;
 };
 
-interface ServerResponse {
+interface ServerResponseArray {
   data: EpisodeProps[];
+}
+
+interface ServerResponseObject {
+  data: EpisodeProps;
 }
 
 export default function CharacterInfo(): JSX.Element {
   const navigate = useNavigate();
   const { t } = useTranslation();
+
   const [episodes, setEpisodes] = useState<EpisodeProps[]>([]);
 
   let { id } = useParams<CharacterParams>();
@@ -28,25 +36,30 @@ export default function CharacterInfo(): JSX.Element {
     id = '1';
   }
   const { data } = useTypeSelector((state) => state.character);
-
   const character = data[+id - 1];
-  // eslint-disable-next-line prefer-const
-  let ids: string[] = [];
 
-  character.episode.map((url) => {
-    const num = url.slice(40);
-    return ids.push(num);
-  });
-
+  const ids: string[] = [];
+  character.episode.map((url) => ids.push(url.slice(40)));
   const pages = ids.join();
 
   const fetchStarringEpisodes = (): EpisodeProps[] => {
-    axios.get(`${paths.episode}/${pages}`).then((response: ServerResponse) => {
-      const episodes: EpisodeProps[] = response.data;
-      console.log(episodes);
+    if (ids.length > 1) {
+      axios
+        .get(`${paths.episode}/${pages}`)
+        .then((response: ServerResponseArray) => {
+          const episodes: EpisodeProps[] = response.data;
+          setEpisodes(episodes);
+        });
+    } else {
+      axios
+        .get(`${paths.episode}/${pages}`)
+        .then((response: ServerResponseObject) => {
+          const episodes: EpisodeProps[] = [];
+          episodes.push(response.data);
+          setEpisodes(episodes);
+        });
+    }
 
-      setEpisodes(episodes);
-    });
     return episodes;
   };
 
@@ -62,18 +75,26 @@ export default function CharacterInfo(): JSX.Element {
             <img src={character.image} alt={character.name} />
           </div>
           <div className={style.Info}>
-            {character.status}
-            {character.species}
-            {character.type}
-            {character.gender}
+            <p>
+              {t('info.status')} <b>{character.status}</b>
+            </p>
+            <p>
+              {t('info.species')} <b>{character.species}</b>
+            </p>
+            <p>
+              {t('info.type')} <b>{character.type}</b>
+            </p>
+            <p>
+              {t('info.gender')} <b>{character.gender}</b>
+            </p>
           </div>
         </div>
         <div className={style.CharacterDescription}>
           <div className={style.Name}>{character.name}</div>
           <div className={style.Description}>
             <div className={style.Links}>
-              Origin: {character.origin.name} <br />
-              <a href={character.episode[0]}>First seen at ...</a>
+              {t('character.origin', { name: character.origin.name })} <br />
+              <a href={character.episode[0]}>{t('character.firstSeen')}</a>
             </div>
             <div className={style.Episodes}>
               <InfiniteScroll
@@ -84,17 +105,18 @@ export default function CharacterInfo(): JSX.Element {
                 height={450}
                 endMessage={<h1>{t('scrollEnd')}</h1>}
               >
-                {episodes.map((item) => (
-                  <EpisodeCard key={item.id} episode={item} />
-                ))}
+                <div className={style.EpisodesContainer}>
+                  {episodes.map((item) => (
+                    <EpisodeCard key={item.id} episode={item} />
+                  ))}
+                </div>
               </InfiniteScroll>
-              Episodes
             </div>
           </div>
         </div>
       </div>
       <div className={style.Button}>
-        <Button label={t('home')} onClick={(): void => navigate('*')} />
+        <Button label={t('back')} onClick={(): void => navigate(-1)} />
       </div>
     </div>
   );
--- src/pages/info/fetchStarringEpisodes.ts
+++ src/pages/info/fetchStarringEpisodes.ts
@@ -3,17 +3,31 @@ import axios from 'config/axios';
 import { EpisodeProps } from 'types/episodeTypes';
 import paths from 'components/navigation/paths';
 
+interface ServerResponseArray {
+  data: EpisodeProps[];
+}
+
+interface ServerResponseObject {
+  data: EpisodeProps;
+}
+
+let episodes: EpisodeProps[] = [];
+
 const fetchStarringEpisodes = (pages: string): EpisodeProps[] => {
-  let episodes: EpisodeProps[] = [];
-  axios
-    .get(paths.episode, {
-      params: {
-        pages,
-      },
-    })
-    .then((response) => {
-      episodes = response.data;
-    });
+  if (pages.length > 1) {
+    axios
+      .get(`${paths.episode}/${pages}`)
+      .then((response: ServerResponseArray) => {
+        episodes = response.data;
+      });
+  } else {
+    axios
+      .get(`${paths.episode}/${pages}`)
+      .then((response: ServerResponseObject) => {
+        episodes.push(response.data);
+      });
+  }
+
   return episodes;
 };
 
